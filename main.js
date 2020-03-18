72c16f140e6ce79ee973a4050a055dfd0d54012a
Umberto Del Piano <umberdelpi1@gmail.com>
2020-03-18 16:34:50 +0100

    Creazione var in main.js

---
+++ main.js
@@ -0,0 +1,11 @@
+// Chiedere all'utente nome_cognome_colore preferito
+
+
+// Creare var per registrare nome utente
+var nomeUtente
+
+// Creare var per registrare cognome utente
+var cognomeUtente
+
+// Creare var per registrare colore preferito utente
+var colorePreferitoUtente
